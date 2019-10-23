bf9204e5ba0e6b67bab20b6c3ecdf0f13f0e1aea
liaodehe <rivernash@163.com>
2019-10-23 14:12:19 +0800

    获取对象后生成insert语句

--- src/main/java/springmvc/controller/WfiWorkflow2bizController.java
+++ src/main/java/springmvc/controller/WfiWorkflow2bizController.java
@@ -1,5 +1,9 @@
 package springmvc.controller;
 
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import springmvc.dao.WfiWorkflow2bizDao;
 import springmvc.model.WfiWorkflow2biz;
 import springmvc.service.WfiWorkflow2bizService;
+import springmvc.util.SqlUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,10 +25,19 @@ public class WfiWorkflow2bizController {
     @Autowired
     WfiWorkflow2bizService wfiWorkflow2bizService;
 
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
     @RequestMapping(value="/WfiWorkflow2biz")
     @ResponseBody
     public WfiWorkflow2biz getWfiWorkflow2biz(HttpServletRequest request, HttpServletResponse response, @RequestParam(required = false) String appl_type) throws Exception {
         WfiWorkflow2biz wfiWorkflow2biz = wfiWorkflow2bizService.getWfiWorkflow2bizDetail(appl_type);
+        Configuration configuration = sqlSessionFactory.getConfiguration();
+        BoundSql boundSql = sqlSessionFactory.getConfiguration()
+                .getMappedStatement("springmvc.dao.WfiWorkflow2bizDao.addWfiWorkflow2biz").getBoundSql(wfiWorkflow2biz);
+
+        String sql = SqlUtils.getSql(configuration, boundSql, "addWfiWorkflow2biz");
+        System.out.println(sql);
         return wfiWorkflow2biz;
     }
 
---
+++ src/main/java/springmvc/dao/interceptor/MybatisInterceptor.java
@@ -0,0 +1,72 @@
+package springmvc.dao.interceptor;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
+import springmvc.util.SqlUtils;
+
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.regex.Matcher;
+
+/**
+ * https://www.cnblogs.com/leavescy/p/7642113.html
+ */
+@Intercepts({
+        @Signature(type = Executor.class, method = "update", args = {
+        MappedStatement.class, Object.class }),
+        @Signature(type = Executor.class, method = "insert", args = {
+                MappedStatement.class, Object.class }),
+        @Signature(type = Executor.class, method = "add", args = {
+                MappedStatement.class, Object.class }),
+        @Signature(type = Executor.class, method = "query", args = {
+                MappedStatement.class, Object.class, RowBounds.class,
+                ResultHandler.class }) })
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class MybatisInterceptor implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        try{
+            MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];  // 获取xml中的一个select/update/insert/delete节点，主要描述的是一条SQL语句
+            Object parameter = null;
+            // 获取参数，if语句成立，表示sql语句有参数，参数格式是map形式
+            if (invocation.getArgs().length > 1) {
+                parameter = invocation.getArgs()[1];
+                System.out.println("parameter = " + parameter);
+            }
+            String sqlId = mappedStatement.getId(); // 获取到节点的id,即sql语句的id
+            System.out.println("sqlId = " + sqlId);
+            BoundSql boundSql = mappedStatement.getBoundSql(parameter);  // BoundSql就是封装myBatis最终产生的sql类
+            Configuration configuration = mappedStatement.getConfiguration();  // 获取节点的配置
+            String sql = SqlUtils.getSql(configuration, boundSql, sqlId); // 获取到最终的sql语句
+            System.out.println("sql = " + sql);
+            //log.debug(sql);
+        }catch(Exception e){
+            // log.error(e.getMessage(), e);
+        }
+        return invocation.proceed();  // 执行完上面的任务后，不改变原有的sql执行过程
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
